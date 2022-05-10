7dcaefe746d394963a431ae8d33a8490bc3722f5
LauraV <lauravilladaa16@gmail.com>
2022-05-09 20:31:04 -0500

    May 9 night

---
+++ src/components/Notes.js
@@ -0,0 +1,6 @@
+const Notes=()=>{
+    return <div className="notes"></div> 
+    <Postit/>
+};
+
+export default Notes;
---
+++ src/components/Postit.js
@@ -0,0 +1,13 @@
+const Postit=()=>{
+    return (
+        <div className="Postit">
+            <span>Hello this is the first note</span>
+            <div className="date-delete">
+                <small>09/05/2022</small>
+            </div>
+
+        </div>
+    )
+}
+
+export default Postit;
