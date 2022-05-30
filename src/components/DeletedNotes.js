3483e6da60662f18cce6000a4f84cd87da635ba9
LauraV <lauravilladaa16@gmail.com>
2022-05-30 16:09:26 -0500

    Notes deleted sent to the trash route

--- src/components/DeletedNotes.js
+++ src/components/DeletedNotes.js
@@ -1,20 +1,133 @@
 import React from "react";
-import Postit from "./Postit";
+import { useDispatch, useSelector } from "react-redux";
+import { MdOutlineRestorePage } from "react-icons/md";
+import { MdDeleteForever } from "react-icons/md";
+// import Postit from "./Postit";
 
-export default function DeletedNotes(note, title, id, editNote, deleteNote) {
-  // const deletedPostits_p = localStorage.getItem("deletedPostits");
-  // const deletedPostits_p = ["hola", "me", "llamo", "lauea"];
+export default function DeletedNotes() {
+  // to take only the date without the time:
+  const current_date = new Date().toLocaleDateString();
+  const deletedList = useSelector((state) => state.note.deletedList);
+  const notesList = useSelector((state) => state.note.notesList);
+  const dispatch = useDispatch();
+  console.log(deletedList);
+  // const [text, setText] = useState("");
+  // const [title, setTitle] = useState("");
+
+  // To take the text in the note:
+  // const noteInfo = (event) => {
+  //   setText(event.target.value);
+  // };
+
+  // // To take the title in the note:
+  // const titleInfo = (event) => {
+  //   setTitle(event.target.value);
+  // };
+
+  // To update the new note
+  // const saveNote = (event) => {
+  //   // to prevent save a note in blank even if the user type spaces
+  //   if (text.trim() !== "") {
+  //     event.preventDefault();
+  //     const newPostit = {
+  //       note: text,
+  //       title: title,
+  //       id: Math.random(), //to generate a unique id
+  //       time: current_date,
+  //     };
+  //     dispatch(addNote(newPostit));
+  //     // to reset the note
+  //     setText("");
+  //     setTitle("");
+  //   }
+  // };
+
+  // { deletedList.map((element) => (
+  //   <div className="postit">
+  //   <textarea
+  //     className="note-title"
+  //     placeholder="Title..."
+  //     maxLength="40"
+  //     onChange={titleInfo}
+  //     value={element.title}
+  //   ></textarea>
+
+  //   <textarea
+  //     className="note-text"
+  //     placeholder="My note..."
+  //     maxLength="230"
+  //     onChange={noteInfo}
+  //     // to associate the state value
+  //     value={element.note}
+  //   ></textarea>
+
+  //   <footer className="note-footer">
+  //     <p id="date">{current_date}</p>
+  //     <MdOutlineRestorePage
+  //       className="button"
+  //       color="#7f7f81"
+  //       size="1.7em"
+  //       onMouseOver={({ target }) => (target.style.color = "#535354")}
+  //       onMouseOut={({ target }) => (target.style.color = "#7f7f81")}
+  //       // onClick={saveNote}
+  //     />
+  //     <MdDeleteForever
+  //       className="button"
+  //       color="#7f7f81"
+  //       size="1.7em"
+  //       onMouseOver={({ target }) => (target.style.color = "#535354")}
+  //       onMouseOut={({ target }) => (target.style.color = "#7f7f81")}
+  //       // onClick={saveNote}
+  //     />
+  //   </footer>
+  // </div>
+  // ))}
   return (
-    <div>
-      <p>me estoy ejecutando</p>
+    // To change the className when there are more than 3 notes
+    // so the align content change from left to center
 
-      <Postit
-        note={note}
-        title={title}
-        id={id}
-        editNote={editNote}
-        deleteNote={deleteNote}
-      />
+    <div className={notesList.length >= 3 ? "multiple-notes" : "few-notes"}>
+      {/* to render all the notes created */}
+
+      {deletedList.map((element) => (
+        <div className="postit">
+          <textarea
+            className="note-title"
+            placeholder="Title..."
+            maxLength="40"
+            value={element.title}
+          ></textarea>
+
+          <textarea
+            className="note-text"
+            placeholder="My note..."
+            maxLength="230"
+            // to associate the state value
+            value={element.note}
+          ></textarea>
+
+          <footer className="note-footer">
+            <p id="date">{current_date}</p>
+            <MdOutlineRestorePage
+              className="button"
+              id="restore-button"
+              color="#7f7f81"
+              size="1.7em"
+              onMouseOver={({ target }) => (target.style.color = "#535354")}
+              onMouseOut={({ target }) => (target.style.color = "#7f7f81")}
+              // onClick={saveNote}
+            />
+            <MdDeleteForever
+              className="button"
+              color="#7f7f81"
+              size="1.7em"
+              onMouseOver={({ target }) => (target.style.color = "#535354")}
+              onMouseOut={({ target }) => (target.style.color = "#7f7f81")}
+              // onClick={saveNote}
+            />
+          </footer>
+        </div>
+      ))}
     </div>
   );
 }
--- src/redux/noteSlice.js
+++ src/redux/noteSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toHaveFormValues } from "@testing-library/jest-dom/dist/matchers";
 
 const getInitialPostits = () => {
   const localNotesList = window.localStorage.getItem("notesList");
-
   // to verify if there are already notes created
   // it is necessary to check the length after parse it
   if (JSON.parse(localNotesList).length > 0) {
@@ -14,9 +14,29 @@ const getInitialPostits = () => {
   return [];
 };
 
+const getDeletedPostits = () => {
+  const localDeletedList = window.localStorage.getItem("deletedList");
+  console.log("localDeletedList");
+  console.log(localDeletedList);
+  // to verify if there are already notes created
+  // it is necessary to check the length after parse it
+  if (JSON.parse(localDeletedList) !== null) {
+    // if (JSON.parse(localDeletedList).length > 0) {
+    // to parse the notes list if there is one in the localStorage
+    console.log("entre al if");
+
+    return JSON.parse(localDeletedList);
+  }
+  // If there are no notes so it has to be created a empty array
+  window.localStorage.setItem("deletedList", JSON.stringify([]));
+
+  return [];
+};
+
 const initialList = {
   // creating a function to obtain the initial postits from the local storage.
   notesList: getInitialPostits(),
+  deletedList: getDeletedPostits(),
 };
 
 export const notesSlice = createSlice({
@@ -37,17 +57,36 @@ export const notesSlice = createSlice({
     deleteNote: (state, action) => {
       const notesList = window.localStorage.getItem("notesList");
       const postitsList = JSON.parse(notesList);
+      const deletedList = window.localStorage.getItem("deletedList");
+      const postitsDeleted = JSON.parse(deletedList);
+      // console.log(postitsDeleted);
       postitsList.forEach((element, index) => {
         if (element.id === action.payload) {
           // to take the note out of the array.
           //the second argument is the amount of elements to take out.
+          // postitsDeleted.push(postitsList.splice(index, 1));
+
+          postitsDeleted.push({
+            note: element.note,
+            title: element.title,
+            id: element.id,
+          });
           postitsList.splice(index, 1);
+          //
         }
       });
       // to update the localStorage
       window.localStorage.setItem("notesList", JSON.stringify(postitsList));
+      window.localStorage.setItem(
+        "deletedList",
+        JSON.stringify(postitsDeleted)
+      );
+
       //to update the state
       state.notesList = postitsList;
+      state.deletedList = postitsDeleted;
+      console.log("los postit eliminados son:");
+      console.log(deletedList);
     },
 
     editNote: (state, action) => {
