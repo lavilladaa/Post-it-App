3be723d108c159952088c6884fee5a2b57b38a08
LauraV <lauravilladaa16@gmail.com>
2022-06-05 17:25:11 -0500

    media query config finished

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -7,7 +7,9 @@ import trash from '../assets/trash.png';
 
 export default function Footer() {
   const deletedList = useSelector((state) => state.note.deletedList);
+  const notesList=useSelector((state) => state.note.notesList);
   const { length } = deletedList;
+  
 
   const alertTrash = () => {
   
@@ -22,7 +24,7 @@ export default function Footer() {
     <>
     {/* to know if there are any postit deleted */}
       {length > 0 ? (
-        <footer className='footer'>
+        <footer className={notesList.length>=4 ? 'footer relative':'footer fixed'}>
           <Link to='/trash'>
             <button className='bg-transparent border-none mb-0 mr-2 bg-bin-full bg-cover h-85 w-85 justify-center items-center cursor-pointer' type="button">
                <div className='count-outline bg-transparent w-8 mx-auto mt-5 p-0 text-2xl font-bold font-chango text-purple-count'>{length}</div>
--- src/components/trash/FooterTrash.jsx
+++ src/components/trash/FooterTrash.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import home from '../assets/home.png';
 
 export default function FooterTrash() {
+  const deletedList = useSelector((state) => state.note.deletedList);
+  
   return (
-    <footer className='footer'>
+    <footer className={deletedList.length>4 ? 'footer relative':'footer fixed'}>
       <Link to='/'>
         <button className='border-none bg-transparent mr-3.5 cursor-pointer' type='button'>
           <img src={home} width='70px' alt='Home Icon' />
